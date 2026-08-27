b7b2b5152873db14e19e3708818e5632b5be3e1a
Rouvus <hpk59kp6cr@privaterelay.appleid.com>
2026-08-27 19:41:10 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -15,7 +15,7 @@
    VERSION wird beim Bauen (build-pwa.sh) automatisch gesetzt. Ändert
    sich die App, ändert sich die Version, und der alte Speicher wird
    verworfen. */
-const VERSION = '383cf415d5cf';
+const VERSION = 'eaed50c6f90d';
 const CACHE = 'koreanisch-' + VERSION;
 const ASSETS = [
   './',
